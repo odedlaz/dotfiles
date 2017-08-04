3dbeb3bd941197e99552a9079d17cc2cfe68bd6c
Oded Lazar <odedlaz@gmail.com>
2017-08-04 12:19:42 +0300

    make ssh-agent work automatically

--- fish/conf.d/ssh-agent.fish
+++ fish/conf.d/ssh-agent.fish
@@ -1,5 +1,7 @@
 if not ps -ef | grep "[s]sh-agent -s" > /dev/null ^&1
    ssh-agent -s > /dev/null ^&1
-   ssh-add $HOME/.ssh/github_rsa ^ /dev/null
-   ssh-add $HOME/.ssh/digitalocean_rsa ^ /dev/null
+   for pubkey in $HOME/.ssh/*.pub
+      set privatekey (echo $pubkey | awk -F '.pub' '{print $1}')
+      ssh-add $privatekey ^ /dev/null
+   end
 end
