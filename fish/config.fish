3dfe880ff66b91ef284ef67ad36113d440bbc1f2
Oded Lazar <odedlaz@gmail.com>
2017-08-09 13:18:23 +0300

    source work functions

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,7 @@
 if status --is-interactive
    fortune -s kernelnewbies linuxcookie linux computers science tao  | ponysay
 end
+
+for path in $HOME/.config/fish/work-functions/*
+   source $path
+end
