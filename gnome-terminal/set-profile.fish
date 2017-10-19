a0646a4b56a054eac46e46ca7d3e8e46334a1929
Oded Lazar <odedlaz@gmail.com>
2017-10-19 11:15:56 +0300

    Add one-dark gnome-shell profile installation

---
+++ gnome-terminal/set-profile.fish
@@ -0,0 +1,97 @@
+#!/usr/bin/env fish
+
+# ONE DARK
+# --- ----
+# Gnome Terminal color scheme install script
+# Based on:
+#   https://github.com/chriskempson/base16-gnome-terminal/
+#
+
+# if powerline fonts are already installed,
+# we assume that one-dark has already been installed.
+
+function install_fonts
+   echo "installing fonts..."
+   # git clone https://github.com/powerline/fonts.git --depth=1 /tmp/fonts
+   # cd /tmp/fonts ;and ./install.sh ;and cd .. ;and rm -rf /tmp/fonts
+end
+
+function dset
+   set key $argv[1]
+   set val $argv[2]
+   dconf write "$PROFILE_KEY/$key" "$val"
+end
+
+set force false
+if count $argv > /dev/null
+   if test $argv[1] = "-f"
+      set force true
+   end
+end
+
+set PROFILE_NAME (whoami)
+set BASE_KEY "/org/gnome/terminal/legacy/profiles:"
+set DEFAULT_SLUG (dconf read $BASE_KEY/default | tr -d \')
+
+set PROFILE_SLUG $PROFILE_NAME
+if which uuidgen > /dev/null
+   set PROFILE_SLUG (uuidgen)
+end
+
+set DEFAULT_KEY "$BASE_KEY/:$DEFAULT_SLUG"
+set PROFILE_KEY "$BASE_KEY/:$PROFILE_SLUG"
+
+set profiles (dconf dump $BASE_KEY/ | \
+              perl -ne '/\[:(.+)\]/ && print "$1\n";' | \
+              grep -v $DEFAULT_SLUG)
+
+if test (count $profiles) -gt 0 ;and test $force = "false"
+   echo "profile already exists"
+end
+
+for profile_id in $profiles
+   set profile_name (dconf read $BASE_KEY/:$profile_id/visible-name)
+   echo "removing profile $profile_name"
+   dconf reset -f $BASE_KEY/:$profile_id/
+end
+
+install_fonts
+
+# copy existing settings from default profile
+dconf dump "$DEFAULT_KEY/" | dconf load "$PROFILE_KEY/"
+dconf reset -f $BASE_KEY/:$DEFAULT_SLUG
+
+dconf write "$BASE_KEY/list" "['$PROFILE_SLUG']"
+dconf write "$BASE_KEY/default" "'$PROFILE_SLUG'"
+
+# update profile values with theme options
+dset visible-name "'$PROFILE_NAME'"
+dset palette "['#000000',
+               '#E06C75',
+               '#98c379',
+               '#d19a66',
+               '#61aeee',
+               '#c678dd',
+               '#56b6c2',
+               '#abb2bf',
+               '#5c6370',
+               '#e06c75',
+               '#98c379',
+               '#d19a66',
+               '#62afee',
+               '#c678dd',
+               '#56b6c2',
+               '#ffffff']"
+dset background-color "'#282c34'"
+dset foreground-color "'#abb2bf'"
+dset bold-color "'#ABB2BF'"
+dset bold-color-same-as-fg "true"
+dset use-theme-colors "false"
+dset use-theme-background "false"
+dset use-custom-command "true"
+dset custom-command "'tmux new -A -s 0'"
+dset use-system-font "false"
+dset font "'Source Code Pro for Powerline Light 12'"
+
+
+echo "profile '$PROFILE_NAME' installed"
