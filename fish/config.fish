22979ddcfb5c898ffc0eba5d5afb1c4fe27d1ef3
Oded Lazar <odedlaz@gmail.com>
2017-10-19 15:02:31 +0300

    Move ponysay greeting to fish_greeting

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,3 @@
-if status --is-interactive
-   fortune -s kernelnewbies linuxcookie linux computers science tao  | ponysay
-end
-
 for dir in functions work-functions
    for path in $HOME/.config/fish/$dir/*
       source $path
---
+++ fish/functions/greeting.fish
@@ -0,0 +1,5 @@
+ function fish_greeting
+   if status --is-interactive
+      fortune -s kernelnewbies linuxcookie linux computers science tao  | ponysay
+   end
+ end
