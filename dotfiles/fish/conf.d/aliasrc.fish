841e8d148cb59d0de775fe9d1d94089e2936efd3
Oded Lazar <odedlaz@gmail.com>
2018-11-26 09:43:39 +0200

    fish: Update aliases

--- dotfiles/fish/conf.d/aliasrc.fish
+++ dotfiles/fish/conf.d/aliasrc.fish
@@ -2,11 +2,7 @@
 set SCRIPTS_DIR "$HOME/Dev/scripts"
 
 alias vi "nvim"
-alias cat "bat --theme TwoDark"
 alias reload "source $HOME/.config/fish/config.fish"
-alias venvoff "deactivate"
 alias readme 'pandoc README.md | lynx -stdin'
 alias pingg "ping google.com"
-alias unpair "/usr/local/bin/doas $SHELL $SCRIPTS_DIR/unpair.sh"
-alias pair "sudo $SHELL $SCRIPTS_DIR/pair.sh"
 alias fuck='eval "sudo $history[1]"'
