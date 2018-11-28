ef81fc72e2e3f9ce5590108209b084df9958e427
Oded Lazar <odedlaz@gmail.com>
2018-11-28 16:26:11 +0200

    fish: update ssh-agent to search for subdirs as well

--- dotfiles/fish/conf.d/ssh-agent.fish
+++ dotfiles/fish/conf.d/ssh-agent.fish
@@ -1,6 +1,6 @@
 if not ps -ef | grep "[s]sh-agent -s" > /dev/null ^&1
    ssh-agent -s > /dev/null ^&1
-   for pubkey in $HOME/.ssh/*.pub
+   for pubkey in (find $HOME/.ssh -t f -name "*.pub")
       set privatekey (echo $pubkey | awk -F '.pub' '{print $1}')
       ssh-add $privatekey ^ /dev/null
    end
