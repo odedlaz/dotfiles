71423c4a6349d0b9acf5b0c016e37524c4641262
Oded Lazar <odedlaz@gmail.com>
2018-11-28 17:51:32 +0200

    i3: update ssh-agent to work correctly with fish

--- dotfiles/fish/conf.d/ssh-agent.fish
+++ dotfiles/fish/conf.d/ssh-agent.fish
@@ -1,7 +1,25 @@
-if not ps -ef | grep "[s]sh-agent -s" > /dev/null ^&1
-   ssh-agent -s > /dev/null ^&1
-   for pubkey in (find $HOME/.ssh -t f -name "*.pub")
-      set privatekey (echo $pubkey | awk -F '.pub' '{print $1}')
-      ssh-add $privatekey ^ /dev/null
+set agent_environ /tmp/.ssh-agent/environment
+
+if not test -f $agent_environ
+   pkill ssh-agent
+end
+
+if not pgrep ssh-agent > /dev/null
+   rm -rf (dirname $agent_environ)
+end
+
+if mkdir (dirname $agent_environ) ^ /dev/null
+   eval (ssh-agent -c | grep '^setenv' | tee $agent_environ)
+   chmod 600 $agent_environ
+
+   for pubkey in (find $HOME/.ssh -type f -name "*.pub")
+      set private_key (echo $pubkey | sed 's/.pub//')
+      ssh-add $private_key ^ /dev/null
    end
 end
+
+source $agent_environ
+
+
+
+
