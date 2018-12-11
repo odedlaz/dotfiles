83009b2327494fda63233d003696be220c0997d3
Oded Lazar <odedlaz@gmail.com>
2018-12-11 10:30:26 +0200

    fish: add a function to copy last executed command

--- dotfiles/fish/functions/pbcopy.fish
+++ dotfiles/fish/functions/pbcopy.fish
@@ -1,3 +1,8 @@
 function pbcopy --description "Copy data from STDIN to the clipboard"
    xclip -selection c
 end
+
+function cmdpbcopy --description "Copy data from STDIN to the clipboard"
+   history -n 1 | pbcopy
+end
+
