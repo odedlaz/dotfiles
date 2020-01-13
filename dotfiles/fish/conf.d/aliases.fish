845481829fd6fc1966cbc341911795e4d671021c
Oded Lazar <odedlaz@gmail.com>
2020-01-13 16:47:57 +0200

    fish: update aliases

--- dotfiles/fish/conf.d/aliases.fish
+++ dotfiles/fish/conf.d/aliases.fish
@@ -1,5 +1,11 @@
 #!/usr/bin/env fish
-alias vi "nvim"
+alias vi /usr/bin/nvim
+alias vi! /usr/bin/vim
+alias ls exa
+alias ls! /bin/ls
+alias grep rg
+alias grep! /bin/grep
+
 alias reload "source $HOME/.config/fish/config.fish"
 alias readme 'pandoc README.md | lynx -stdin'
 alias pingg "ping google.com"
