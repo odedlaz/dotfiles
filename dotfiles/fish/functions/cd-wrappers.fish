056bb236f51135c9e33d569521f0774efbaa605e
Oded Lazar <odedlaz@gmail.com>
2020-01-20 12:49:52 +0200

    fish: replace xfd with *-wrappers

--- dotfiles/fish/functions/cd-wrappers.fish
+++ dotfiles/fish/functions/cd-wrappers.fish
@@ -10,35 +10,9 @@ function cdfd --argument text
    cd "$path"; or return 1
 end
 
-function vifd --argument text
-   set path (fd -t f "$text" | fzf)
-   if test -n "$path"
-      vi "$path"
-   end
-end
-
-function vilocate --argument text
-   set path (locate "$text" | fzf)
-   if test -n "$path"
-      vi "$path"
-   end
-end
-
 function cdlocate --argument text
    set path (locate "$text" | fzf)
    cdto "$path"
-end
-
-function virg --argument text
-   set match (rg "$text" --vimgrep --no-messages | fzf)
-
-   if test -z "$match"
-      return 1
-   end
-
-   set cursor_cmd (echo "$match" | awk -F ':' '{ print "+normal "$2"G"$3"|" }')
-   set path (echo "$match" | awk -F ':' '{ print $1 }')
-   nvim "$cursor_cmd" $path
 end
 
 function cdto --argument path
@@ -56,11 +30,6 @@ function cdto --argument path
 
    cd $path
 end
-
-function viwhich --argument binary
-   vi (which $binary)
-end
-
 
 function cdrg --argument text
    set match (rg "$text" --vimgrep --no-messages | fzf)
